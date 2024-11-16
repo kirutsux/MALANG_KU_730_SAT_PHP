9e4550e1eebe35a5bf63780eeff33b949eaf4ef9
Kurtz Malang <kurtz.malang@gmail.com>
2024-11-16 22:50:09 +0800

    1116_Vid28_Techniques-for-Building-Tailwind-Projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "black": "#060606"
+            },
+            fontFamily: {
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
+            },
+            fontSize: {
+                "2xs": ".625rem" // 10px
+            }
+        },
+    },
+    plugins: [],
 }
