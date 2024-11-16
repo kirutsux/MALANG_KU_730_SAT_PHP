9e84d6fa714bb53402a16a09e831c89f91ab2830
Kurtz Malang <kurtz.malang@gmail.com>
2024-11-16 23:24:24 +0800

    1116_Vid30_Wrap-Up

--- routes/web.php
+++ routes/web.php
@@ -2,39 +2,25 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
-use App\Jobs\TranslateJob;
-use App\Models\Job;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('test', function () {
-    $job = Job::first();
+Route::get('/', [JobController::class, 'index']);
 
-    TranslateJob::dispatch($job);
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
 
-    return 'Done';
+Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
 });
 
-Route::view('/', 'home');
-Route::view('/contact', 'contact');
-
- Route::get('/jobs', [JobController::class, 'index']);
- Route::get('/jobs/create', [JobController::class, 'create']);
- Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
- Route::get('/jobs/{job}', [JobController::class, 'show']);
-
- Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
-     ->middleware('auth')
-     ->can('edit', 'job');
-
- Route::patch('/jobs/{job}', [JobController::class, 'update']);
- Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-// Auth
-Route::get('/register', [RegisteredUserController::class, 'create']);
-Route::post('/register', [RegisteredUserController::class, 'store']);
-
-Route::get('/login', [SessionController::class, 'create'])->name('login');
-Route::post('/login', [SessionController::class, 'store']);
-Route::post('/logout', [SessionController::class, 'destroy']);
-
+Route::delete('/logout', [SessionController::class, 'destroy'])->middleware('auth');
