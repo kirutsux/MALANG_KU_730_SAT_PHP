4ba0466eae2dea3df370d49e0ba893d50b676745
Kurtz Malang <kurtz.malang@gmail.com>
2024-11-16 22:48:03 +0800

    1116_Vid27_From-Design-to-Blade

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,6 +2,7 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
+    
 
     <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
@@ -10,7 +11,7 @@
     </p>
 
     @can('edit', $job)
-        <p class="mt-6">
+        <p clasJobs="mt-6">
            <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
         </p>
     @endcan
